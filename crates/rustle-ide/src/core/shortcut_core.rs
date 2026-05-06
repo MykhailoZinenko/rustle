e27a2a1d5ab1717d732650db7c0ddeb4ac0b7cb2
Igor Poprushko <48104058+IgorPoprushko@users.noreply.github.com>
2026-05-06 17:11:35 +0200

    Ctrl to command

--- crates/rustle-ide/src/core/shortcut_core.rs
+++ crates/rustle-ide/src/core/shortcut_core.rs
@@ -12,22 +12,22 @@ pub enum ShortcutAction {
 
 pub fn consume_shortcut(ctx: &Context) -> Option<ShortcutAction> {
     ctx.input_mut(|input| {
-        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::CTRL, Key::S)) {
+        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::COMMAND, Key::S)) {
             return Some(ShortcutAction::Save);
         }
-        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::CTRL, Key::N)) {
+        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::COMMAND, Key::N)) {
             return Some(ShortcutAction::NewFile);
         }
-        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::CTRL, Key::O)) {
+        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::COMMAND, Key::O)) {
             return Some(ShortcutAction::OpenFile);
         }
-        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::CTRL, Key::W)) {
+        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::COMMAND, Key::W)) {
             return Some(ShortcutAction::CloseTab);
         }
         if input.consume_key(Modifiers::NONE, Key::F5) {
             return Some(ShortcutAction::ToggleRun);
         }
-        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::CTRL, Key::Space)) {
+        if input.consume_shortcut(&KeyboardShortcut::new(Modifiers::COMMAND, Key::Space)) {
             return Some(ShortcutAction::ToggleSuggestions);
         }
         None
