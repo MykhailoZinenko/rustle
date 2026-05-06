bb311007bedd8f7653c6aa766cbbd9f464fba3db
MykhailoZinenko <mzinenko.work@gmail.com>
2026-05-06 12:54:45 +0200

    feat(renderer): instance data types with vertex layouts

---
+++ crates/rustle-renderer/src/instance.rs
@@ -0,0 +1,51 @@
+use bytemuck::{Pod, Zeroable};
+
+#[repr(C)]
+#[derive(Copy, Clone, Pod, Zeroable)]
+pub struct SdfInstance {
+    pub center: [f32; 2],
+    pub size: [f32; 2],
+    pub color: [f32; 4],
+    pub shape_params: [f32; 4],
+    pub line_dir: [f32; 4],
+}
+
+impl SdfInstance {
+    pub const ATTRIBS: [wgpu::VertexAttribute; 5] = wgpu::vertex_attr_array![
+        0 => Float32x2,
+        1 => Float32x2,
+        2 => Float32x4,
+        3 => Float32x4,
+        4 => Float32x4,
+    ];
+
+    pub fn layout() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as u64,
+            step_mode: wgpu::VertexStepMode::Instance,
+            attributes: &Self::ATTRIBS,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Pod, Zeroable)]
+pub struct PolygonVertex {
+    pub position: [f32; 2],
+    pub color: [f32; 4],
+}
+
+impl PolygonVertex {
+    pub const ATTRIBS: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![
+        0 => Float32x2,
+        1 => Float32x4,
+    ];
+
+    pub fn layout() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as u64,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS,
+        }
+    }
+}
